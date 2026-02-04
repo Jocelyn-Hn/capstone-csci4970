769e3072b53bfceab3fa0d134db4e5f37a0175f2
Josef Stanek <jstanek459@gmail.com>
2026-02-04 14:43:27 -0600

    Added Comments to Generate_Aruco.py

--- Generate_Aruco.py
+++ Generate_Aruco.py
@@ -3,15 +3,19 @@ import cv2.aruco as aruco
 import numpy as np
 
 def generate_markers():
-
+    #Define the dictionary we want to use for marker generation
     aruco_dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
 
+    #Create only 2 markers (should only need the two for the windows max)
     for marker_id in range(2):
         
+        #Define marker size, 200 pixels should be enough
         marker_size = 200
 
+        #Create the actual marker image
         marker_image = aruco.generateImageMarker(aruco_dictionary, marker_id, marker_size)
         
+        #Store the marker in the dedicated AruCo_markers folder, name it based off its ID
         cv.imwrite(f"AruCo_Markers/aruco_{marker_id}.png", marker_image)
         
 
