2370546ddec00a993c3c97540676a04c157743c8
Josef Stanek <jstanek459@gmail.com>
2026-03-10 17:50:43 -0500

    Canvas & Video stream setup
    finalized accessCamera() a JS script that prompts the user for camera access before showcasing their video feed back to them onscreen. From there frames can be drawn to the canvas at whatever fps wanted. Updated calibrate camera to utilize this script.
    
    NOTE: Calibrate camera no longer functions because of this, in future pushes this will be remedied.

--- painlessprep/core/static/painlessprep/accessCamera.js
+++ painlessprep/core/static/painlessprep/accessCamera.js
@@ -1,12 +1,68 @@
 const video = document.getElementById("cameraStream");      //Grab the <video> block with id cameraStream 
 const canvas = document.getElementById("canvas");           //grab the <canvas> block where the video will be output
+const ctx = canvas.getContext("2d");
+let intervalActive;
+let fps = 15;
 
+
+
+// navigator.mediaDevices.getUserMedia({video : true})
+//         .then(stream => {video.srcObject = stream;}) //Pass stream in as the video source object
+//         .catch(err => {console.error("Camera Error: ", err);} //if an error occurs, pass that error to the console error
+//         );
 
 //Prompt the user to give access to the camera, then, stream that camera to the given <video> HTML block
-navigator.mediaDevices.getUserMedia({video : true})
-        .then(stream => {video.srcObject = stream;}) //Pass stream in as the video source object
-        .catch(err => {console.error("Camera Error: ", err);} //if an error occurs, pass that error to the console error
-        );
+async function startVideo()
+{
+        try{
+                //Pass stream in as the video source object
+                const stream = await navigator.mediaDevices.getUserMedia({video : true});
+                video.srcObject = stream;
+        } 
+        catch (err){
+                //if an error occurs, pass that error to the console error
+                console.error("Camera Error: ",err);
+        }
+}
+
+function captureFrame()
+{
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+
+        ctx.drawImage(video,0,0);
+        
+}
+
+function beginCapture()
+{
+        if(!intervalActive)
+        {
+                intervalActive = setInterval(captureFrame, 1000/fps);
+        }
+        else
+        {
+                alert("Capturing has already begun.");
+        }
+        
+}  
+
+function stopCapture()
+{
+    if(!intervalActive)
+        {
+                alert("Capture has not been started.");
+        }
+        else
+        {
+                clearInterval(intervalActive);
+                intervalActive = null;
+                ctx.clearRect(0,0,canvas.width,canvas.height);
+        }    
+}
+
+startVideo();
+
 
 
 
