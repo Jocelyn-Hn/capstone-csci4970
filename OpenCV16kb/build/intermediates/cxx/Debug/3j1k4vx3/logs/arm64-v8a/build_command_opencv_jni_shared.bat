7f194d1c493bab2cdfa124aabb1a34fbdb2fae38
Josef Stanek <jstanek459@gmail.com>
2026-04-20 17:09:02 -0500

    Removed unnecessary opencv build

--- OpenCV16kb/build/intermediates/cxx/Debug/3j1k4vx3/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\mario\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\mario\\Desktop\\School stuff\\Capstone - Painless Prep\\Github\\capstone-csci4970\\OpenCV16kb\\.cxx\\Debug\\3j1k4vx3\\arm64-v8a" ^
-  opencv_jni_shared
