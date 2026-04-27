e0fc3735383b9a273015bce83bcc57fff5094372
Josef Stanek <jstanek459@gmail.com>
2026-04-27 18:49:14 -0500

    Removed files
    Cleared files from the dokka branch for future importing of documentation

--- html/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies='{":app/main":[],":app/release":[]}'
--- opencv/build/intermediates/cxx/Debug/1s2h1u5w/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\katho\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\katho\\AndroidStudioProjects\\capstone-csci4970\\opencv\\.cxx\\Debug\\1s2h1u5w\\arm64-v8a" ^
-  opencv_jni_shared
